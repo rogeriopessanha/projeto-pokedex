5aa43e18d5264ab37638a9b70e4d953e77cd2fbb
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-02-11 05:16:29 -0300

    att

---
+++ pokedex-app/src/components/ListPokemons.vue
@@ -0,0 +1,22 @@
+
+<script setup>
+const pokemon = defineProps(['name', 'urlBaseSvg'])
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card mb-3">
+            <p class="text-center">{{ pokemon.name }}</p>
+            <img 
+            :src="urlBaseSvg" class="card-img-top" alt="..."
+            height="60"
+            >
+        </div>
+    </div>
+</template>
+
+
+<style>
+
+</style>
--- pokedex-app/src/views/HomeView.vue
+++ pokedex-app/src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { onMounted, reactive, ref } from 'vue';
+import ListPokemons from '../components/ListPokemons.vue'
+
+let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
 
 let pokemons = reactive(ref())
 
-onMounted(() =>{
+onMounted(() => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=251&offset=0")
-  .then(res => res.json())
-  .then(res =>  pokemons.value = res.results)
+    .then(res => res.json())
+    .then(res => pokemons.value = res.results)
 })
 </script>
 
@@ -15,24 +18,28 @@ onMounted(() =>{
     <div class="container">
       <div class="row mt-4">
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
+
+          <!-- <div class="card" style="width: 18rem;">
             <img src="../assets/pngegg.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                 content.</p>
-              
             </div>
-          </div>
+          </div> -->
+
         </div>
 
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;" >
-            <ul>
-              <li v-for="pokemon in pokemons" :key="pokemon.name">
-                {{ pokemon.name }}
-              </li>
-            </ul>
+          <div class="card">
+
+            <div class="card-body row">
+              <ListPokemons v-for="pokemon in pokemons" 
+              :key="pokemon.name" 
+              :name="pokemon.name"
+              :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+              />
+            </div>
           </div>
         </div>
       </div>
