9a454d481113d82c8e2d1f7b4ca9546b1730f10d
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-02-11 05:59:46 -0300

    att

--- pokedex-app/src/components/ListPokemons.vue
+++ pokedex-app/src/components/ListPokemons.vue
@@ -6,17 +6,27 @@ const pokemon = defineProps(['name', 'urlBaseSvg'])
 
 <template>
     <div class="col-4">
-        <div class="card mb-3">
+        <div class="card mb-3 cardListPokemon">
             <p class="text-center">{{ pokemon.name }}</p>
-            <img 
-            :src="urlBaseSvg" class="card-img-top" alt="..."
-            height="60"
-            >
+            <img :src="urlBaseSvg" class="card-img-top" alt="..." height="60">
         </div>
     </div>
 </template>
 
 
-<style>
+<style scoped>
+.cardListPokemon {
+    background: rgb(67,82,84);
+background: linear-gradient(90deg, rgba(67,82,84,0.47911547911547914) 0%, rgba(47,73,52,0.28501228501228504) 79%);
+cursor: pointer;
+}
 
+.cardListPokemon:hover {
+    background: rgb(67,82,84);
+    background: linear-gradient(90deg, rgba(67,82,84,0.47911547911547914) 0%, rgba(47,73,52,0.773955773955774) 79%);
+}
+
+.cardListPokemon img:hover {
+    padding: .1rem;
+}
 </style>
--- pokedex-app/src/views/HomeView.vue
+++ pokedex-app/src/views/HomeView.vue
@@ -1,16 +1,27 @@
 <script setup>
-import { onMounted, reactive, ref } from 'vue';
+import { onMounted, reactive, ref, computed } from 'vue';
 import ListPokemons from '../components/ListPokemons.vue'
 
 let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
 
 let pokemons = reactive(ref())
+let searchPokemonField = ref('')
 
 onMounted(() => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=251&offset=0")
     .then(res => res.json())
     .then(res => pokemons.value = res.results)
 })
+
+const pokemonsFiltered = computed(() => {
+  if (pokemons.value && searchPokemonField.value) {
+    return pokemons.value.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(searchPokemonField.value.toLowerCase())
+    )
+  }
+  return pokemons.value
+})
+
 </script>
 
 <template>
@@ -34,11 +45,18 @@ onMounted(() => {
           <div class="card">
 
             <div class="card-body row">
-              <ListPokemons v-for="pokemon in pokemons" 
-              :key="pokemon.name" 
-              :name="pokemon.name"
-              :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
-              />
+
+              <div class="mb-3">
+
+                <label hidden for="searchPokemonField" class="form-label">Pesquisar...</label>
+
+                <input v-model="searchPokemonField" type="text" class="form-control" id="searchPokemonField"
+                  placeholder="Procure por um pokemon">
+
+              </div>
+
+              <ListPokemons v-for="pokemon in pokemonsFiltered" key="pokemon.name" :name="pokemon.name"
+                :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'" />
             </div>
           </div>
         </div>
